620ac00abf3ae8d5a509f83a62bc53da3b23cf06
Jkuk1 <klimosej@vscht.cz>
2024-02-21 10:03:50 +0100

    commiting a forest roads script to generate terrain waves

---
+++ my_tools.py
@@ -0,0 +1,12 @@
+from qgis.core import QgsProcessingAlgorithm
+from qgis.core import QgsApplication  # Add this line if QgsApplication is needed in your function
+from qgis.core import QgsRasterLayer
+from qgis.analysis import QgsNativeAlgorithms  # Add this line if needed
+from datetime import datetime
+
+import os 
+import processing
+import string
+import random 
+import time
+
